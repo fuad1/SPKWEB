aa16f4925bc21db23be79c4b5260d8ba07bf45a6
fuad1 <48826495+fuad1@users.noreply.github.com>
2020-07-05 22:34:17 +0700

    Add files via upload

---
+++ SPKWEB/index.php
@@ -0,0 +1,43 @@
+<html>
+<title>SPK</title>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="#" class="navbar-brand">SPK</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a href="saw.php" class="button">SAW</a>
+                <a href="wp.php" class="button">WP</a>
+                <a href="ahp.php" class="button">AHP</a>
+                <a href="electree.php" class="button">ELECTREE</a>   
+        </div>
+    </nav>
+    <br>
+    <center>
+    <h3>TUGASWEB SPK</h3>
+    <hr>
+    <table>
+        <tr>
+            <th>Nama</th><th>:</th><th>Muhammad Fuad Nurhadi</th>
+        </tr>
+        <tr>
+            <th>NIM</th><th>:</th><th>1741720102</th>
+        </tr>
+        <tr>
+            <th>Kelas</th><th>:</th><th>TI - 3C</th>
+        </tr>
+    </table>
+    </center>
+    
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>

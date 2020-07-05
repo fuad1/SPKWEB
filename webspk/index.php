5a428ef72d1f93c80ac1db3c6a81608c8bbb7f12
fuad1 <48826495+fuad1@users.noreply.github.com>
2020-07-05 16:32:58 +0700

    Add files via upload

---
+++ webspk/index.php
@@ -0,0 +1,66 @@
+<html>
+<title>WEB_SPK</title>
+<head>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Simple Sidebar - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/simple-sidebar.css" rel="stylesheet">
+</head>
+<body>
+   <div class="d-flex" id="wrapper">
+		<a href="index.php" class="navbar-brand">SPK</a>
+                <a href="saw.php" class="button">SAW</a>
+                <a href="wp.php" class="button">WP</a>
+                <a href="electree.php" class="button">ELECTREE</a>
+                <a href="topsis.php" class="button">TOPSIS</a>
+                <a href="moora.php" class="button">MOORA</a>
+                <a href="gdss.php" class="button">GDSS</a>
+            </div>
+        </div>
+    </nav>
+    <br>
+    <center>
+    <h2>TUGAS WEB SISTEM PENDUKUNG KEPUTUSAN</h2>
+    <table>
+	
+    <th>Nama</th><th>:</th><th>Muhammad Fuad Nurhadi</th>
+    </tr>
+    <tr>
+    <th>NIM</th><th>:</th><th>1741720102</th>
+    </tr>
+	   <tr>
+      <th>Absen</th><th>:</th><th>16</th>
+      </tr>
+	</table>
+	
+	<table>
+	      <tr>
+        <h2><th>JURUSAN TEKNOLOGI INFORMASI</th></h2>
+        </tr>
+        <tr>
+        <h2><th>PROGRAM STUDI TEKNIK INFORMATIKA</th></h2>
+        </tr>
+	       <tr>
+          <h2><th> POLITEKNIK NEGERI MALANG</th></h2>
+          </tr>
+          <tr>
+          <h2><th>2020</th></h2>
+        </tr>
+	</table>
+    	  
+    </center>
+    
+
+    <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
